c363dc3a715af07a9b94ee0efe91cea651e492bf
Franklin ONP <3110022770701@ingenieria.usac.edu.gt>
2024-01-30 18:37:43 -0600

    interfaz
    Peque;o avance en la interfaz, aun faltan muchas funcionalidades pero ya tiene la estructura basica de como va a ser  :smiley:

--- Proyecto1_Compi1_C_202200089/src/main/Proyecto1_Compi1_C_202200089.java
+++ Proyecto1_Compi1_C_202200089/src/main/Proyecto1_Compi1_C_202200089.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package main;
+import GUI.Principal;
 
 /**
  *
@@ -15,6 +16,15 @@ public class Proyecto1_Compi1_C_202200089 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+
+        Principal miInterfaz = new Principal();
+        // Hacer visible la interfaz
+        miInterfaz.setVisible(true);
+        
+    //    EditorDeTexto et= new EditorDeTexto();
+    //    et.setVisible(true);
+        
+       
     }
     
 }
